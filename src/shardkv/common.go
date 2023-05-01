560335f614102aa6c8db05b8238ef383275f3237
Khighness <1823676372@qq.com>
2023-05-02 00:39:38 +0800

    Lab4-ShardKV(0502): Shard model.

--- src/shardkv/common.go
+++ src/shardkv/common.go
@@ -24,10 +24,43 @@ const (
 	MethodAppend = "Append"
 )
 
+type Status int
+
+const (
+	StatusDefault = iota
+	StatusPull
+	StatusPush
+	StatusCollection
+)
+
 // Shard structure.
 type Shard struct {
 	Status int
 	Data   map[string]string
+}
+
+// Get returns the value according to the key.
+func (s *Shard) Get(key string) string {
+	return s.Data[key]
+}
+
+// Put adds a key-value pair.
+func (s *Shard) Put(key string, value string) {
+	s.Data[key] = value
+}
+
+// Append appends a value for a key.
+func (s *Shard) Append(key string, value string) {
+	s.Data[key] += value
+}
+
+// Copy returns the copied data.
+func (s *Shard) Copy() map[string]string {
+	result := make(map[string]string)
+	for k, v := range s.Data {
+		result[k] = v
+	}
+	return result
 }
 
 // KVCommandRequest structure.
@@ -47,16 +80,38 @@ type KVCommandResponse struct {
 
 // FetchShardRequest structure.
 type FetchShardRequest struct {
+	Num       int
+	ShardList []int
 }
 
 // FetchShardResponse structure.
 type FetchShardResponse struct {
+	Err    Err
+	Num    int
+	State  map[int]Shard
+	Client map[int64]int
+}
+
+// FetchShard structure.
+type FetchShard struct {
+	Num    int
+	State  map[int]Shard
+	Client map[int64]int
 }
 
 // CleanShardRequest structure.
 type CleanShardRequest struct {
+	Num       int
+	ShardList []int
 }
 
 // CleanShardResponse structure.
 type CleanShardResponse struct {
+	Err Err
+}
+
+// CleanShard structure.
+type CleanShard struct {
+	ShardList []int
+	Num       int
 }
